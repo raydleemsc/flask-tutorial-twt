1e818162e6f680f0bad23a12a2ee4f8acc031e35
Raymond D. Lee, M. Sc <raydlee.msc@gmail.com>
2021-08-14 23:10:17 +0700

    database check and creation update

--- website/__init__.py
+++ website/__init__.py
@@ -17,7 +17,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from . import models
+    from .models import User, Note
 
     create_database(app)
 
