6ae7223e651483c6c295e10e71b8be8f5b93831e
Raymond D. Lee, M. Sc <raydlee.msc@gmail.com>
2021-08-14 23:35:45 +0700

    added login

--- website/auth.py
+++ website/auth.py
@@ -7,8 +7,19 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login' ,methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully.', category='success')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html")
 
 @auth.route('/logout')
