46cdcff870a5f31646786e09018ba3565cdf224a
Raymond D. Lee, M. Sc <raydlee.msc@gmail.com>
2021-09-06 12:18:04 +0700

    check for venv path before activating

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import os
 
 activate_this = '/var/www/vhosts/raydlee.org.uk/flask-twt.raydlee.org.uk/env-flask/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
+if os.path.exists(activate_this):
+    with open(activate_this) as file_:
+        exec(file_.read(), dict(__file__=activate_this))
 
 import sys
 import logging
